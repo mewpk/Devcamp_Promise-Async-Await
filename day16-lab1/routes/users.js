060ef237974406de6ecbb7dbf3b10e807be8585e
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-17 11:14:33 +0700

    save

--- day16-lab1/routes/users.js
+++ day16-lab1/routes/users.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql2')
+var mysql = require('mysql2/promise')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // <== ระบุให้ถูกต้อง
-    password: '1234', // <== ระบุให้ถูกต้อง
-    database: 'codecamp', // <== ระบุ database ให้ถูกต้อง
-    port: 3306, // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
-  });
+  mysql
+    .createConnection({
+      host: 'localhost',
+      user: 'root', // <== ระบุให้ถูกต้อง
+      password: '1234', // <== ระบุให้ถูกต้อง
+      database: 'codecamp', // <== ระบุ database ให้ถูกต้อง
+      port: 3306, // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
+    })
+    .then((connection) => {
+
+      const userPromise = connection.query('SELECT * from user');
+      const classroomPromise = connection.query('SELECT * from classroom');
  
-  // เปิด connection ไปที่ database
-  connection.connect();
+      Promise.all([userPromise, classroomPromise])
+        .then((rows) => {
+          res.json({
+            users: rows[0][0],
+            classsrooms: rows[1][0],
+          });
+        })
+        .catch((err) => {
+          res.json({ error: err });
+        });
  
-  console.log('start query at: ' + new Date().getTime());
-  connection.query('SELECT * from user', (err, rows, fields) => {
-    if (err) throw err;
- 
-    console.log('end query at: ' + new Date().getTime());
-    res.json(rows);
-  });
- 
-  // ปิด connection
-  connection.end();
+    });
  });
+ 
  
 module.exports = router;
