3504baea31872b69563e9fbc3f2a9a6482828839
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-17 16:33:43 +0700

    save

--- homework2/frontend/src/Components/User.js
+++ homework2/frontend/src/Components/User.js
@@ -1,33 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import axios from "axios"
 
 export default function User() {
+
+    const [users,setUsers] = useState(null)
+
+
+
+    const get = async () => {
+        const response = await axios.get("http://localhost:3000/api/users")
+        setUsers(response.data) 
+        console.log(users)
+    };
+
     useEffect(() => {
-        const get = async () => {
-            const response = await axios.get("http://localhost:3000/api/users")
-            const data = await response.data
-            data.map((x) => {
-                const output = document.getElementById('output')
-                output.innerHTML +=  `<tr>
-                <td>id : ${x.id}</td>
-                <td>firstname : ${x.firstname}</td>
-                <td>lastname : ${x.lastname}</td>
-                <td>salary : ${x.salary}</td>
-                </tr>`
-            })
+
+        if(!users){
+            get()
         }
-        get()
-
-
-
-
+        
     }, [])
+   
 
-    return (
+    return ( 
         <div>
+        
             <table>
                 <tbody id='output'>
+                {
+                   
+                   users &&  users.map((x) => {
+                        return (
+                            <tr>
+                            <td>id : {x.id}</td>
+                            <td>firstname : {x.firstname}</td>
+                            <td>lastname : {x.lastname}</td>
+                            <td>salary : {x.salary}</td>
+                            </tr>
+                        )
+                        
+                    })
                 
+                   
+                   
+                }
                 </tbody>
 
             </table>
